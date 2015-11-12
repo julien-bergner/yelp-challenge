f5c00255ebce17f5f5535e7a15ed4d303e4f9028
Julien Bergner <julien@bergner.fr>
2015-11-12 02:43:12 +0100

    Update filter_by_language script

--- scripts/filter_by_language.py
+++ scripts/filter_by_language.py
@@ -1,5 +1,8 @@
 #!/usr/local/bin/python3
 
+# This scripts reads the reviews, detects their language and writes
+# then to target_file_en or target_file_de depending on their language.
+
 import sys
 import json
 import pdb
@@ -8,12 +11,13 @@ from langdetect import detect
 from langdetect.lang_detect_exception import LangDetectException
 
 if len(sys.argv) != 4:
-    sys.exit("Usage: filter_py_language.py source_file target_file iso_of_language_to_filter_for")
+    sys.exit("Usage: filter_py_language.py source_file target_file_en target_file_de")
 
 print("Start: ", str(datetime.now()))
 
 source_file = open(sys.argv[1], "r")
-target_file = open(sys.argv[2], "w")
+target_file_en = open(sys.argv[2], "w")
+target_file_de = open(sys.argv[3], "w")
 print("Source file: ", source_file.name)
 print("Target file: ", target_file.name)
 
@@ -26,11 +30,16 @@ with source_file as source:
 
         try:
             language = detect(review_text)
+            line = { "review_id" : review_id, "text" : review_text }
 
-            if language == sys.argv[3]:
-                line = { "review_id" : review_id, "text" : review_text }
-                target_file.write(json.dumps(line))
-                target_file.write('\n')
+            if language == 'en':
+                target_file_en.write(json.dumps(line))
+                target_file_en.write('\n')
+            elif language == 'de':
+                target_file_de.write(json.dumps(line))
+                target_file_de.write('\n')
+            else:
+                print("Other language: ", language, parsed_json)
 
         except LangDetectException:
             pass
